dd51630ac151eb6a886dcbbc36d762a9dc15f59e
yeonghun <126738945+jyh11224@users.noreply.github.com>
2023-07-26 14:52:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -63,6 +63,113 @@ def plot_power_plants(df_combined, num_plots):
     plt.tight_layout()
     st.pyplot(fig)
 
+############################ 수정필요 ####################################
+df_combined["hour"] = df_combined['datetime'].dt.hour
+df_pivot = df_combined.pivot(index='datetime', columns='발전소코드', values='gap')
+df_pivot = df_pivot.reset_index()
+df_pivot["hour"] = df_pivot['datetime'].astype("datetime64").dt.hour
+df_pivot
+df_pivot = df_pivot.drop(columns=['datetime']).groupby('hour').mean()
+df_pivot
+
+# 발전소 코드 리스트
+plant_list = df_error['발전소코드'].tolist()
+
+# 특성 스케일링
+hours = df_pivot.index
+scaler = StandardScaler()
+
+scaled_error_data = scaler.fit_transform(df_pivot)
+
+
+df_scaled = pd.DataFrame(scaled_error_data, columns=df_pivot.columns)
+df_scaled.set_index(hours, inplace=True)
+df_T_scaled = df_scaled.T
+
+max_clusters = 20
+
+inertia = []
+silhouette_scores = []
+for k in range(2, max_clusters+1):
+    e_kmeans = KMeans(n_clusters=k)
+    e_kmeans.fit(df_T_scaled)
+    inertia.append(e_kmeans.inertia_) # ELBOW
+
+    # Silhouette 계수 계산
+    s_labels = e_kmeans.labels_
+    silhouette_avg = silhouette_score(df_T_scaled, s_labels)
+    silhouette_scores.append(silhouette_avg)
+
+fig, ax1 = plt.subplots()
+
+ax1.plot(range(2, max_clusters+1), inertia, 'bo-')
+ax1.set_xlabel('Number of Clusters')
+ax1.set_ylabel('Inertia')
+ax1.set_title('Elbow Method')
+
+# Silhouette 그래프 출력
+ax2 = ax1.twinx()
+ax2.plot(range(2, max_clusters+1), silhouette_scores, 'rs-')
+ax2.set_ylabel('Silhouette Score', color='r')
+ax2.tick_params('y', colors='r')
+
+st.pyplot(fig)
+
+
+# K-means 알고리즘을 사용하여 클러스터링 모델 학습
+kmeans = KMeans(n_clusters=10, init='k-means++')  # 클러스터 개수는 상황에 맞게 설정합니다
+kmeans.fit(df_T_scaled)
+
+km = TimeSeriesKMeans(n_clusters=7, metric="euclidean")
+
+labels = km.fit_predict(df_T_scaled)
+# 학습된 클러스터링 모델로 데이터를 예측 (각 데이터 포인트가 속한 클러스터를 예측)
+
+cluster_count = math.ceil(math.sqrt(len(set(labels))))
+# A good rule of thumb is choosing k as the square root of the number of points in the training data set in kNN
+
+labels
+score = silhouette_score(df_T_scaled, labels)
+print(score)
+
+plot_count = math.ceil(math.sqrt(len(set(labels))))
+
+
+fig, axs = plt.subplots(plot_count,plot_count,figsize=(10,10))
+fig.suptitle('Clusters')
+row_i=0
+column_j=0
+# For each label there is,
+# plots every series with that label
+for label in set(labels):
+    cluster = []
+    for i in range(len(labels)):
+            if(labels[i]==label):
+                axs[row_i, column_j].plot([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23], df_scaled[plant_list[i]].values ,c="gray",alpha=0.4)
+                cluster.append(df_scaled[plant_list[i]].values)
+    if len(cluster) > 0:
+        axs[row_i, column_j].plot(np.average(np.vstack(cluster),axis=0),c="red")
+    axs[row_i, column_j].set_title("Cluster "+str(row_i*plot_count+column_j))
+    axs[row_i, column_j].set_xlim(0, 23)
+    column_j+=1
+    if column_j%plot_count == 0:
+        row_i+=1
+        column_j=0
+
+st.pyplot(fig)
+
+
+cluster_c = [len(labels[labels==i]) for i in range(len(set(labels)))]
+cluster_n = ["Cluster "+str(i) for i in range(len(set(labels)))]
+plt.figure(figsize=(15,10))
+plt.title("Cluster Distribution for KMeans")
+plt.bar(cluster_n,cluster_c)
+st.bar_chart(cluster_C)
+
+fancy_names_for_labels = [f"Cluster {label}" for label in labels]
+p = pd.DataFrame(zip(plant_list,fancy_names_for_labels),columns=["plant code","Cluster"]).sort_values(by="Cluster").set_index("plant code")
+p = p.sort_index()
+####################################################################
 
 def main():
     # Read and preprocess data
